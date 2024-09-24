c8561dce0740e7527d7883bcaaaf0359240d2b09
biggaboy212 <75142294+biggaboy212@users.noreply.github.com>
2024-09-23 22:33:42 -0500

    Update configTest.lua

--- testReleases/configTest.lua
+++ testReleases/configTest.lua
@@ -4547,8 +4547,8 @@ function MacLib:Window(Settings)
 							Description = string.format("Created config %q", inputPath),
 						})
 
+						configSelection:ClearOptions()
 						configSelection:InsertOptions(MacLib:RefreshConfigList())
-						configSelection:UpdateSelection(nil)
 					end,
 				})
 
@@ -4593,8 +4593,8 @@ function MacLib:Window(Settings)
 				configSection:Button({
 					Name = "Refresh Config List",
 					Callback = function()
+						configSelection:ClearOptions()
 						configSelection:InsertOptions(MacLib:RefreshConfigList())
-						configSelection:UpdateSelection(nil)
 					end,
 				})
 			end
